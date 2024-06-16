7402f5e5a89cd171d2e51d454202203697800851
Roman <myakunovre@yandex.ru>
2024-06-16 13:53:12 +0300

    Изменить имя параметра в параметризированном классе TaskLinkedList

--- src/manager/TaskLinkedList.java
+++ src/manager/TaskLinkedList.java
@@ -3,14 +3,14 @@ package manager;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TaskLinkedList<Task> {
-    public Node<Task> first;
-    public Node<Task> last;
+public class TaskLinkedList<T> {
+    public Node<T> first;
+    public Node<T> last;
     int size = 0;
 
-    void linkLast(Task task) {
-        final Node<Task> oldLast = last;
-        final Node<Task> newNode = new Node<>(oldLast, task, null);
+    void linkLast(T task) {
+        final Node<T> oldLast = last;
+        final Node<T> newNode = new Node<>(oldLast, task, null);
         last = newNode;
         if (oldLast == null)
             first = newNode;
@@ -19,32 +19,32 @@ public class TaskLinkedList<Task> {
         size++;
     }
 
-    List<Task> getTasks() {
-        List<Task> list = new ArrayList<>();
+    List<T> getTasks() {
+        List<T> list = new ArrayList<>();
 
-        for (Node<Task> x = first; x != null; x = x.next) {
+        for (Node<T> x = first; x != null; x = x.next) {
             list.add(x.item);
         }
 
         return list;
     }
 
-    void removeNode(Node<Task> node) {
+    void removeNode(Node<T> node) {
         if (node.prev != null && node.next != null) { // если нода - не голова и не хвост списка
-            Node<Task> prevNode = node.prev;
-            Node<Task> nextNode = node.next;
+            Node<T> prevNode = node.prev;
+            Node<T> nextNode = node.next;
             prevNode.next = nextNode;
             nextNode.prev = prevNode;
             size--;
         }
         if (node.prev == null && node.next != null) { // если нода - голова списка
-            Node<Task> nextNode = node.next;
+            Node<T> nextNode = node.next;
             first = nextNode;
             nextNode.prev = null;
             size--;
         }
         if (node.prev != null && node.next == null) { // если нода - хвост списка
-            Node<Task> prevNode = node.prev;
+            Node<T> prevNode = node.prev;
             last = prevNode;
             prevNode.next = null;
             size--;
